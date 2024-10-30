49a7c0a0f283eecede6641fd1313d5ef0c1c7683
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2024-10-30 20:24:13 +0530

    Technologies added and colour fixed

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { RiReactjsLine } from "react-icons/ri";
-import { SiExpress, SiNextdotjs } from "react-icons/si";
-import { SiMongodb } from "react-icons/si";
-import { IoLogoNodejs } from "react-icons/io";
-import { FaJs, FaNodeJs } from "react-icons/fa";
+import {
+  SiExpress,
+  SiMongodb,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiMysql,
+  SiGit,
+} from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
 import { DiJava } from "react-icons/di";
 
 const Technologies = () => {
@@ -11,23 +16,49 @@ const Technologies = () => {
     <div className="border-b border-neutral-800 pb-24">
       <h2 className="my-20 text-center text-4xl">Technologies</h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
+        {/* React */}
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <RiReactjsLine className="text-7xl text-cyan-400" />
+          <RiReactjsLine className="text-7xl" style={{ color: "#61DBFB" }} />
         </div>
+
+        {/* Express */}
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <SiExpress className="text-7xl text-green-400" />
+          <SiExpress className="text-7xl text-green-500" />
         </div>
+
+        {/* Node.js */}
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <FaNodeJs className="text-7xl text-green-500" />
+          <FaNodeJs className="text-7xl" style={{ color: "#68A063" }} />
         </div>
+
+        {/* MongoDB */}
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <SiMongodb className="text-7xl text-green-500" />
+          <SiMongodb className="text-7xl" style={{ color: "#4DB33D" }} />
         </div>
+
+        {/* Next.js */}
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <SiNextdotjs className="text-7xl" />
+          <SiNextdotjs className="text-7xl text-white" />
         </div>
+
+        {/* Java */}
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <DiJava className="text-7xl text-red-500" />
+          <DiJava className="text-7xl" style={{ color: "#f89820" }} />
+        </div>
+
+        {/* Tailwind CSS */}
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiTailwindcss className="text-7xl" style={{ color: "#38BDF8" }} />
+        </div>
+
+        {/* SQL */}
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiMysql className="text-7xl" style={{ color: "#00758F" }} />
+        </div>
+
+        {/* Git */}
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiGit className="text-7xl" style={{ color: "#F05032" }} />
         </div>
       </div>
     </div>
